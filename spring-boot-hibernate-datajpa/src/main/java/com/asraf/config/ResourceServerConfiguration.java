ff1e93fbd595c3894dc82c15cf26a1f2946e00d3
Asraf Uddin Ahmed <aahmed@ejogajogbd.com>
2019-04-16 18:12:20 +0600

    Anonymous user can access all GET requests

--- spring-boot-hibernate-datajpa/src/main/java/com/asraf/config/ResourceServerConfiguration.java
+++ spring-boot-hibernate-datajpa/src/main/java/com/asraf/config/ResourceServerConfiguration.java
@@ -6,7 +6,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
-import org.springframework.core.env.Environment;
 import org.springframework.http.HttpMethod;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -24,27 +23,29 @@ import com.asraf.properties.AuthProperties;
 @EnableResourceServer
 public class ResourceServerConfiguration extends ResourceServerConfigurerAdapter {
 	@Autowired
-    private AuthProperties authProperties;
-    
+	private AuthProperties authProperties;
+
 	private static final String RESOURCE_ID = null;
-    private static final String SECURED_PATTERN = "/**";
+	private static final String SECURED_PATTERN = "/**";
 
-    @Override
-    public void configure(ResourceServerSecurityConfigurer resources) {
-        resources.resourceId(RESOURCE_ID).tokenServices(tokenServices());
-    }
+	@Override
+	public void configure(ResourceServerSecurityConfigurer resources) {
+		resources.resourceId(RESOURCE_ID).tokenServices(tokenServices());
+	}
 
-    @Override
-    public void configure(HttpSecurity http) throws Exception {
-        http.requestMatchers()
-                .antMatchers(SECURED_PATTERN).and().authorizeRequests()
-                .antMatchers(HttpMethod.POST, SECURED_PATTERN).access(ScopeTypes.WRITE)
-                .antMatchers(HttpMethod.PUT, SECURED_PATTERN).access(ScopeTypes.WRITE)
-                .antMatchers(HttpMethod.DELETE, SECURED_PATTERN).access(ScopeTypes.DELETE)
-                .anyRequest().access(ScopeTypes.READ);
-    }
-    
-    /* For resource server configuration with auth server */
+	@Override
+	public void configure(HttpSecurity http) throws Exception {
+		http.requestMatchers().antMatchers(SECURED_PATTERN).and().authorizeRequests()
+				.antMatchers(HttpMethod.POST, SECURED_PATTERN).access(ScopeTypes.WRITE)
+				.antMatchers(HttpMethod.PUT, SECURED_PATTERN).access(ScopeTypes.WRITE)
+				.antMatchers(HttpMethod.DELETE, SECURED_PATTERN).access(ScopeTypes.DELETE)
+				.anyRequest()
+				.anonymous()
+				//.access(ScopeTypes.READ)
+				;
+	}
+
+	/* For resource server configuration with auth server */
 //    @Primary
 //	@Bean
 //	public RemoteTokenServices tokenServices() {
@@ -54,31 +55,31 @@ public class ResourceServerConfiguration extends ResourceServerConfigurerAdapter
 //		tokenService.setClientSecret(authProperties.getClientSecret());
 //		return tokenService;
 //	}
-    
-    /* For resource server configuration without auth server */
-    @Bean
-    @Primary
-    public DefaultTokenServices tokenServices() {
-        final DefaultTokenServices defaultTokenServices = new DefaultTokenServices();
-        defaultTokenServices.setTokenStore(tokenStore());
-        return defaultTokenServices;
-    }
 
-    /* JDBC token store configuration */
+	/* For resource server configuration without auth server */
+	@Bean
+	@Primary
+	public DefaultTokenServices tokenServices() {
+		final DefaultTokenServices defaultTokenServices = new DefaultTokenServices();
+		defaultTokenServices.setTokenStore(tokenStore());
+		return defaultTokenServices;
+	}
 
-    @Bean
-    public DataSource authDataSource() {
-        final DriverManagerDataSource dataSource = new DriverManagerDataSource();
-        dataSource.setDriverClassName(authProperties.getJdbc().getDriverClassName());
-        dataSource.setUrl(authProperties.getJdbc().getUrl());
-        dataSource.setUsername(authProperties.getJdbc().getUsername());
-        dataSource.setPassword(authProperties.getJdbc().getPassword());
-        return dataSource;
-    }
+	/* JDBC token store configuration */
 
-    @Bean
-    public TokenStore tokenStore() {
-        return new JdbcTokenStore(authDataSource());
-    }
-    
+	@Bean
+	public DataSource authDataSource() {
+		final DriverManagerDataSource dataSource = new DriverManagerDataSource();
+		dataSource.setDriverClassName(authProperties.getJdbc().getDriverClassName());
+		dataSource.setUrl(authProperties.getJdbc().getUrl());
+		dataSource.setUsername(authProperties.getJdbc().getUsername());
+		dataSource.setPassword(authProperties.getJdbc().getPassword());
+		return dataSource;
+	}
+
+	@Bean
+	public TokenStore tokenStore() {
+		return new JdbcTokenStore(authDataSource());
+	}
+
 }
