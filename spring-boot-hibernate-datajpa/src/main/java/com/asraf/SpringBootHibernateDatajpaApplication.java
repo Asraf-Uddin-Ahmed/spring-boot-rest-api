79e6057d4a1ce817a47982e190b04ffde3c951a4
Asraf Uddin Ahmed <13ratul@gmail.com>
2019-07-30 12:33:01 +0600

    Default catalog set for resource DB

--- spring-boot-hibernate-datajpa/src/main/java/com/asraf/SpringBootHibernateDatajpaApplication.java
+++ spring-boot-hibernate-datajpa/src/main/java/com/asraf/SpringBootHibernateDatajpaApplication.java
@@ -2,11 +2,13 @@ package com.asraf;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
+import com.asraf.entities.Student;
 import com.asraf.properties.AuthProperties;
 import com.asraf.properties.AwsProperties;
 import com.asraf.properties.FileStorageProperties;
@@ -16,6 +18,7 @@ import com.asraf.repositories.persistence.ExtendedQueryDslJpaRepositoryImpl;
 @EnableScheduling
 @EnableJpaRepositories(repositoryBaseClass = ExtendedQueryDslJpaRepositoryImpl.class)
 @EnableConfigurationProperties({ FileStorageProperties.class, AwsProperties.class, AuthProperties.class })
+@EntityScan(basePackageClasses = { Student.class })
 public class SpringBootHibernateDatajpaApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
--- spring-boot-hibernate-datajpa/src/main/java/com/asraf/models/security/UserDetailsModel.java
+++ spring-boot-hibernate-datajpa/src/main/java/com/asraf/models/security/UserDetailsModel.java
@@ -7,7 +7,7 @@ import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
-import com.asraf.entities.User;
+import com.asraf.entities_auth.User;
 
 public class UserDetailsModel implements UserDetails {
 
